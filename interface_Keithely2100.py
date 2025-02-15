9b8edd637a6cff870a587b22e5c2a5a44d621e15
adeshazer123 <adeshazer@wisc.edu>
2025-02-14 23:11:42 -0600

    Update __init__ and commit_settings

--- interface_Keithely2100.py
+++ interface_Keithely2100.py
@@ -6,6 +6,28 @@ class Keithley2100(Device):
 
     voltage = Cpt(SignalRO, value=0.0, name="voltage")
     mode = Cpt(Signal, value="VOLT:DC", name="mode")
+
+    params = { [
+        {
+            "title": "Resources",
+            "name": "resources",
+            "type": "str",
+            "value": "USB0::0x05E6::0x2100::1149087::INSTR",
+        },
+        {
+            "title": "Keithley2100 Parameters",
+            "name": "K2100Params",
+            "type": "group",
+            "children": [
+                {"title": "ID", "name": "ID", "type": "text", "value": ""},
+                {
+                    "title": "Mode",
+                    "name": "mode",
+                    "type": "list",
+                    "limits": ["VDC", "VAC", "R2W", "R4W", "IDC", "IAC"], 
+                    "value": "VDC",
+                },
+            ] } ] }
     
     def __init__(
         self,
@@ -18,5 +40,16 @@ class Keithley2100(Device):
         parent=None,
         child_name_separator="_",
         **kwargs,
-): super().__init__(name=name, parent=parent, kind=kind, **kwargs)
+    ): 
+        super().__init__(name=name, parent=parent, kind=kind, **kwargs)
+        self._driver = Keithley()
+        self._driver.open_resource(self.resources)
+        self.mode.put(self.params["K2100Params"]["mode"]["value"])
+        self._driver.set_mode(self.mode)
     
+    def read(self):
+        return self._driver.read()
+    
+    def commit_settings(self):
+        if self.mode.get() != self.params["K2100Params"]["mode"]["value"]:
+            self._driver.set_mode(self.params["K2100Params"]["mode"]["value"])
