791d057356b37d3d87e5f3952fc8dd2ffabeb0f9
nano713 <50872819+nano713@users.noreply.github.com>
2025-02-19 23:02:44 +0900

    Update read, init, mode, and import visa_driver.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- interface_Keithely2100.py
+++ interface_Keithely2100.py
@@ -1,14 +1,18 @@
 import logging
 
 from ophyd import Device, Component as Cpt, Signal, SignalRO
-from pymeasure.instruments.keithley.keithley2000 import Keithley2000 #Keithley2000 works for Keithley2100
+# from pymeasure.instruments.keithley.keithley2000 import Keithley2000 
+from keithley2100_VISADriver import Keithley2100VISADriver as Keithley
+import threading
+import time
+# #Keithley2000 works for Keithley2100
 # import pyvisa
 
 logger = logging.getLogger(__name__)
 
 class Keithley2100(Device):
 
-    voltage = Cpt(Signal, name="voltage")
+    voltage = Cpt(Signal, name="voltage", value = 0.0)
     mode = Cpt(Signal, value="VOLT:DC", name="mode")
 
     params = {
@@ -28,9 +32,9 @@ class Keithley2100(Device):
                     "title": "Mode",
                     "name": "mode",
                     "type": "list",
-                    "limits": ["VDC", "VAC", "R2W", "R4W", "IDC", "IAC"],
+                    "limits": ["VOLT:DC", "VOLT:AC", "RES", "FRES", "CURR:DC", "CURR:AC"],
                     # ValueError: Value of voltage` is not in the discrete set {'current': 'CURR:DC', 'current ac': 'CURR:AC', 'voltage': 'VOLT:DC', 'voltage ac': 'VOLT:AC', 'resistance': 'RES', 'resistance 4W': 'FRES', 'period': 'PER', 'frequency': 'FREQ', 'temperature': 'TEMP', 'diode': 'DIOD', 'continuity': 'CONT'}
-                    "value": "VDC",
+                    "value": "VOLT:DC",
                 },
             ],
         },
@@ -49,9 +53,22 @@ class Keithley2100(Device):
         **kwargs,
     ): 
         super().__init__(name=name, parent=parent, kind=kind, **kwargs)
-        self._driver = Keithley2000(self.params["resources"]["value"])
+        self._driver = Keithley(self.params["resources"]["value"])
+        self._driver.init_hardware()
         # self.voltage.get(self._driver.voltage)
-        self.voltage.get = lambda: self._driver.voltage
+        # self.voltage.get = lambda: self.read()
+        
+        # self.voltage.get = lambda: self.read()
+        #create a lambda read function that will be called when the voltage signal is read
+        #         self.V_DC.query = lambda: self.query_function("VOLT:DC")
+
+        self.voltage.put(self.read())
+        
+        mode_string = next((item for item in self.params["K2100Params"]["children"] if item["name"] == "mode"))
+        self.mode.put(mode_string["value"])
+        self._driver.set_mode(self.mode.get())
+
+        
 
         # self.mode.put(self.params["K2100Params"]["children"][1]["value"])
 
@@ -72,15 +89,51 @@ class Keithley2100(Device):
     #                                        )    
     
     def read(self):
-        return self._driver.read()
+        voltage = self._driver.read()   
+        self.voltage.put(voltage)
+        print(f"Voltage: {voltage}")
+        return voltage
+        # self.voltage.put(self._driver.read())
+        # return self.voltage.get()
 
     def commit_settings(self):
-        if self.mode.get() != self.params["K2100Params"]["mode"]["value"]:
-            self._driver.set_mode(self.params["K2100Params"]["mode"]["value"])
-            self.mode.put(self.params["K2100Params"]["mode"]["value"])
+        mode_str = next(item for item in self.params["K2100Params"]["children"] if item["name"] == "mode")
+        if self.mode.get() != mode_str["value"]:
+            self._driver.set_mode(mode_str["value"])
+            self.mode.put(mode_str["value"])
+        # elif self.voltage.get() != self.params["voltage"]["value"]:
+        #     self.voltage.put(self.voltage.get())
+            
     
     def trigger(self):
-        self._driver.trigger()
+        self._driver.read()
     
     def measure_voltage(self):
         self.voltage.put(self._driver.measure_voltage())
+
+#     def _contonious_read(self):
+#         while not self._stop_event.is_set():
+#             self.read()
+#             time.sleep(1)
+    
+#     def start_continuous_read(self):
+#         if self._thread is None:
+#             self._stop_event.clear()
+#             self._thread = threading.Thread(target=self._contonious_read)
+#             self._thread.start()
+    
+#     def stop_continuous_read(self):
+#         if self._thread is not None:
+#             self._stop_event.set()
+#             self._thread.join()
+#             self._thread = None
+
+# if __name__ == "__main__":
+#     # logging.basicConfig(level=logging.INFO)
+#     keithley = Keithley2100(name="keithley2100")
+#     keithley.start_continuous_read()
+#     try:
+#         while True:
+#             time.sleep(1)
+#     except KeyboardInterrupt:
+#         keithley.stop_continuous_read()
