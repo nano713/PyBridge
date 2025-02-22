add7c20ce389080048d3de4c8567f2835fb7782b
nano713 <50872819+nano713@users.noreply.github.com>
2025-02-22 19:55:27 +0900

    Add hinted in kind of voltage

--- interface_Keithely2100.py
+++ interface_Keithely2100.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 
 class Keithley2100(Device):
 
-    voltage = Cpt(Signal, name="voltage", value = 0.0)
-    mode = Cpt(Signal, value="VOLT:DC", name="mode")
+    voltage = Cpt(SignalRO, kind="hinted")
+    mode = Cpt(Signal, value="VOLT:DC", kind="config")
 
     params = {
         "resources": {
@@ -56,17 +56,13 @@ class Keithley2100(Device):
         self._driver = Keithley(self.params["resources"]["value"])
         self._driver.init_hardware()
         # self.voltage.get(self._driver.voltage)
-        # self.voltage.get = lambda: self.read()
-        
-        # self.voltage.get = lambda: self.read()
-        #create a lambda read function that will be called when the voltage signal is read
-        #         self.V_DC.query = lambda: self.query_function("VOLT:DC")
+        # self.voltage.get = lambda: self._driver.read()
+        self.voltage.get = lambda: self.measure()
 
-        self.voltage.put(self.read())
-        
         mode_string = next((item for item in self.params["K2100Params"]["children"] if item["name"] == "mode"))
         self.mode.put(mode_string["value"])
         self._driver.set_mode(self.mode.get())
+        self.trigger()
 
         
 
@@ -75,22 +71,10 @@ class Keithley2100(Device):
         # self.mode.put(self.params["K2100Params"]["mode"]["value"])
         # self._driver.set_mode(self.mode)
 
-        # self.voltage.put(self._driver.read())
-        
-    # def init_hardware(self):
-    #     """Initialize the selected VISA resource
-        
-    #     :param pyvisa_backend: Expects a pyvisa backend identifier or a path to the visa backend dll (ref. to pyvisa)
-    #     :type pyvisa_backend: string
-    #     """
-    #     self.rm = pyvisa.highlevel.ResourceManager()
-    #     self._instr = self.rm.open_resource(self.params["resources"]["value"],``
-    #                                        write_termination="\n",
-    #                                        )    
     
-    def read(self):
+    def measure(self):
         voltage = self._driver.read()   
-        self.voltage.put(voltage)
+        # self.voltage.put(voltage)
         print(f"Voltage: {voltage}")
         return voltage
         # self.voltage.put(self._driver.read())
@@ -105,8 +89,8 @@ class Keithley2100(Device):
         #     self.voltage.put(self.voltage.get())
             
     
-    def trigger(self):
-        self._driver.read()
+    # def trigger(self):
+    #     self._driver.read()
     
     def measure_voltage(self):
         self.voltage.put(self._driver.measure_voltage())
@@ -137,3 +121,32 @@ class Keithley2100(Device):
 #             time.sleep(1)
 #     except KeyboardInterrupt:
 #         keithley.stop_continuous_read()
+
+if __name__ == "__main__":
+    from interface_Keithely2100 import Keithley2100
+    keithley = Keithley2100(name="keithley")
+
+    from bluesky import RunEngine
+
+    RE = RunEngine({})
+    from bluesky.callbacks.best_effort import BestEffortCallback
+
+    bec = BestEffortCallback()
+
+    # Send all metadata/data captured to the BestEffortCallback.
+    RE.subscribe(bec)
+
+    from bluesky.utils import ProgressBarManager
+
+    RE.waiting_hook = ProgressBarManager()
+
+    from bluesky.plans import count
+
+    # from ophyd.sim import det1, det2  # two simulated detectors
+    # dets = [det1, det2]   # a list of any number of detectors
+    # RE(count(dets))
+
+    # dets = [keithley.voltage]  # a list of any number of detectors
+
+    dets = [keithley]
+    RE(count(dets))
