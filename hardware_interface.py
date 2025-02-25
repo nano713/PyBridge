3fcad4f47210fc46cf604df6d887f33c42641a4c
nano713 <50872819+nano713@users.noreply.github.com>
2025-02-25 23:04:38 +0900

    Add get_position, home, move_relative, and close methods. Edit position component. Add bluesky.
    with @adeshazer123

--- hardware_interface.py
+++ hardware_interface.py
@@ -19,22 +19,22 @@ from shrc203_VISADriver import SHRC203VISADriver as SHRC
 logger = logging.getLogger(__name__)
 
 
-class SHRCStage(PVPositioner):
+class SHRCStage(Device):
     # DK - or PVPositioner, which one is better?
+    
     axis_component = Cpt(Signal, value=1)
-    speed_ini = Cpt(Signal, value=10000)
-    speed_fin = Cpt(Signal, value=10000)
+    speed_ini = Cpt(Signal, value=2000)
+    speed_fin = Cpt(Signal, value=20000)
     accel_t = Cpt(Signal, value=100)
-    setpoint = Cpt(SignalRO, value=0.0)
+    store_position = Cpt(Signal, value=0.0, kind="hinted")
     loop = Cpt(Signal, value=0)
     unit = Cpt(Signal, value="um")
-    done = Cpt(Signal, value=False)
     # DK add done and done_value property
 
     params = {
         "visa_name": {
             "title": "Instrument Address:",
-            "type": "str",
+            "type": "valuestr",
             "value": "ASRL3::INSTR",
         },
         "unit": {
@@ -54,78 +54,57 @@ class SHRCStage(PVPositioner):
         },
     }
     axis_int = {"X": 1, "Y": 2, "Z": 3}
-
+    
     def __init__(
-        self,
-        prefix="",
-        *,
-        limits=True,
-        name=None,
-        read_attrs=None,
-        configuration_attrs=None,
-        parent=None,
-        egu=" ",
-        **kwargs,
-    ):
-        super().__init__(
-            prefix=prefix,
-            read_attrs=read_attrs,
-            configuration_attrs=configuration_attrs,
-            name=name,
-            parent=parent,
+            self,
+            prefix="",
+            *,
+            name,
+            parent=None, kind=None,
             **kwargs,
-        )        
-        # super().__init__(
-        #     prefix=prefix,
-        #     name=name,
-        #     kind=kind,
-        #     read_attrs=read_attrs,
-        #     configuration_attrs=configuration_attrs,
-        #     parent=parent,
-        #     **kwargs,
-        # )
+    ):
+        super().__init__(name=name, parent=parent, kind=kind, **kwargs)
         
         self.stage = SHRC(self.params["visa_name"]["value"])
         self.stage.open_connection()
         self.stage.set_unit(self.params["unit"]["value"])
         self.axis_component.put(self.axis_int[self.params["axis"]["value"]])
         self._egu = self.params['unit']['value']
-        self.done.put(False)
         # self.setpoint = self.stage.get_position(self.axis_component.get())
 
-    # def set_axis(self, axis):
-    #     if axis in self.axis_int:
-    #         self.axis_component.put(axis.value)
-    #     else:
-    #         self.axis_component.put(axis.value)
-    #         logger.warning("Invalid axis. Defaulting to axis X")
+    def set_axis(self, axis):
+        if axis in self.axis_int.values():
+            self.axis_component.put(axis)
+        else:
+            self.axis_component.put(1)
+            logger.warning("Invalid axis. Defaulting to axis X")
 
     # def get_axis(self):
     #     return self.axis_component.get()
 
-    # def commit_settings(self):
-    #     if (
-    #         self.params["speed_ini"]
-    #         and self.params["speed_fin"]
-    #         and self.params["accel_t"] is not None
-    #     ):
-    #         self.speed_ini.put(self.params["speed_ini"]["value"])
-    #         self.speed_fin.put(self.params["speed_fin"]["value"])
-    #         self.accel_t.put(self.params["accel_t"]["value"])
-    #         self.stage.set_speed(
-    #             self.params["speed_ini"]["value"],
-    #             self.params["speed_fin"],
-    #             self.params["accel_t"],
-    #             self.axis_component.get(),
-    #         )
+    def commit_settings(self):
+        if (
+            self.params["speed_ini"]
+            and self.params["speed_fin"]
+            and self.params["accel_t"] is not None
+        ):
+            self.speed_ini.put(self.params["speed_ini"]["value"])
+            self.speed_fin.put(self.params["speed_fin"]["value"])
+            self.accel_t.put(self.params["accel_t"]["value"])
+            self.stage.set_speed(
+                self.params["speed_ini"]["value"],
+                self.params["speed_fin"],
+                self.params["accel_t"],
+                self.axis_component.get(),
+            )
 
-    #     elif self.params["unit"] is not None:
-    #         self.unit.put(self.params["unit"])
-    #         self.stage.set_unit(self.params["unit"])
+        elif self.params["unit"] is not None:
+            self.unit.put(self.params["unit"])
+            self.stage.set_unit(self.params["unit"])
 
-    #     elif self.params["loop"] is not None:
-    #         self.loop.put(self.params["loop"])
-    #         self.stage.set_loop(self.params["loop"])
+        elif self.params["loop"] is not None:
+            self.loop.put(self.params["loop"])
+            self.stage.set_loop(self.params["loop"])
 
     # def move(self, position: float, wait=True, timeout=None):
     #     self.stage.move(position, self.axis_component.get())
@@ -135,18 +114,26 @@ class SHRCStage(PVPositioner):
     
     def move(self, position: float, wait=True, timeout=None):
         self.stage.move(position, self.axis_component.get())
-        self.done.put(self.stage.get_done())
+        self.store_position.put(self.get_position())
+    
+    def get_position(self): 
+        return self.stage.query_position(self.axis_component.get())
+    
+    def home(self):
+        self.stage.home(self.axis_component.get())
+        self.store_position.put(self.get_position())
+        logger.debug("Actuators have moved home")
         
-        done = MoveStatus(positioner=self, target=position)
-        
-        if self.done.get():
-            done.set_finished()
-        else:
-            done.set_exception(RuntimeError("Move operation failed"))
-        
-        return done
-
-        # if self.get_position() == position:
+    def move_relative(self, position):
+        self.target_position = position
+        position = self.target_position - self.get_position()
+ 
+        self.stage.move_relative(position, self.axis_component.get())
+ 
+    def close(self):
+       self.stage.close()
+ 
+         # if self.get_position() == position:
         #     logger.info(f"Stage moved to position {position}")
         # else:
         #     logger.error(f"Stage failed to move to position {position}")
@@ -191,3 +178,58 @@ class SHRCStage(PVPositioner):
 
 
 # For PYQT5, we need to load widgets and text to have the commit_settings to load in the GUI
+if __name__ == "__main__":
+    from hardware_interface import SHRCStage
+    from bluesky import RunEngine
+    from bluesky.callbacks.best_effort import BestEffortCallback
+    from bluesky.utils import ProgressBarManager
+    from bluesky.plans import count
+    from bluesky.callbacks import LiveTable, LivePlot
+    from ophyd.sim import motor
+    from bluesky.plans import scan
+    import matplotlib.pyplot as plt
+    import h5py
+
+    plt.ion()
+    shrc = SHRCStage(name="shrc203")
+
+    RE = RunEngine({})
+    bec = BestEffortCallback()
+    RE.subscribe(bec)
+    RE.waiting_hook = ProgressBarManager()
+
+    from databroker import Broker
+    db = Broker.named("temp")
+    
+    RE.subscribe(db.insert)
+    # live_plot = LivePlot('keithley_voltage', 'motor')
+    # RE.subscribe(live_plot)
+
+    # token = RE.subscribe(LiveTable([keithley]))
+    # RE(count([keithley], num=5, delay=0.1))
+    RE(scan([shrc], motor, -1, 1, 10))
+
+    header = db[-1]
+    df = header.table()
+
+    # header = db[-1]
+    # df = header.table()
+    metadata = header.start
+
+    df.to_hdf("data.h5", key = "df", mode = "w")
+    # with h5py.File("data_with.h5", "a") as f:
+    #     for key, value in metadata.items():
+    #         f.attrs[key] = value
+    
+    print("Data saved to data.h5")
+    # plt.show(block = True)
+
+
+    # dets = [keithley]
+    # RE(one_run_one_event(dets))
+
+    # print(f"keithley.read(): {keithley.read()}")
+    # print(f"keithley.get(): {keithley.get()}")
+    # print(f"keithley.voltage: {keithley.voltage.get()}")
+    plt.show()
+    
