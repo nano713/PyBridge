5db906b7efcca57ee89f04dcf796b9b7f5f3338a
nano713 <50872819+nano713@users.noreply.github.com>
2025-02-26 13:52:29 +0900

    Update load_settings, initUI, and init. Add pyvisa.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- GUI_SHRC.py
+++ GUI_SHRC.py
@@ -1,47 +1,58 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 from hardware_interface import SHRCStage
+import logging
+import pyvisa
 
 class MyWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.rm = pyvisa.ResourceManager()
         self.initUI()
-        self.stage = SHRCStage()
+        self.shrc = SHRCStage(name='shrc203')
     
     def initUI(self): 
         layout = QtWidgets.QVBoxLayout()
 
-        self.visa_name = QtWidgets.QLineEdit('Instrument Address:') 
-        self.visa_input = QtWidgets.QLineEdit()
+        self.visa_name = QtWidgets.QLabel('Instrument Address:') 
+        self.visa_input = QtWidgets.QComboBox()
+        self.visa_input.addItems(self.rm.list_resources())
         layout.addWidget(self.visa_name)
         layout.addWidget(self.visa_input)
 
-        self.unit_name = QtWidgets.QLineEdit('Unit:')
+        self.unit_name = QtWidgets.QLabel('Unit:')
         self.unit_input = QtWidgets.QComboBox()
         self.unit_input.addItems(["um", "mm", "nm", "deg", "pulse"])
         layout.addWidget(self.unit_name)
         layout.addWidget(self.unit_input)
 
-        self.loop_name = QtWidgets.QLineEdit('Loop:')
-        self.loop_input = QtWidgets.QSpinBox()
+        self.loop_name = QtWidgets.QLabel('Loop:')
+        self.loop_input = QtWidgets.QComboBox()
+        self.loop_input.addItems(["0", "1"])
         layout.addWidget(self.loop_name)
         layout.addWidget(self.loop_input)
 
-        self.speed_ini_name = QtWidgets.QLineEdit('Speed Initial:')
+        self.speed_ini_name = QtWidgets.QLabel('Speed Initial:')
         self.speed_ini_input = QtWidgets.QDoubleSpinBox()
+        self.speed_ini_input.setRange(0, 100000)
+        self.speed_ini_input.setValue(2000)
         layout.addWidget(self.speed_ini_name)
         layout.addWidget(self.speed_ini_input)
 
-        self.accel_t_name = QtWidgets.QLineEdit('Acceleration Time:')   
+        self.accel_t_name = QtWidgets.QLabel('Acceleration Time:')   
         self.accel_t_input = QtWidgets.QDoubleSpinBox()
+        self.accel_t_input.setRange(0, 100000)
+        self.accel_t_input.setValue(100)
         layout.addWidget(self.accel_t_name)
         layout.addWidget(self.accel_t_input)
 
-        self.speed_fin_name = QtWidgets.QLineEdit('Speed Final:')
+        self.speed_fin_name = QtWidgets.QLabel('Speed Final:')
         self.speed_fin_input = QtWidgets.QDoubleSpinBox()
+        self.speed_fin_input.setRange(0, 100000)
+        self.speed_fin_input.setValue(20000)
         layout.addWidget(self.speed_fin_name)
         layout.addWidget(self.speed_fin_input)
 
-        self.axis_name = QtWidgets.QLineEdit('Axis:')
+        self.axis_name = QtWidgets.QLabel('Axis:')
         self.axis_input = QtWidgets.QComboBox()
         self.axis_input.addItems(["X", "Y", "Z"])
         layout.addWidget(self.axis_name)
@@ -51,13 +62,24 @@ class MyWindow(QtWidgets.QWidget):
         self.load_button.clicked.connect(self.load_settings) # DK - AttributeError: 'MyWindow' object has no attribute 'load_settings'
         layout.addWidget(self.load_button)
 
-        self.commit_button = QtWidgets.QPushButton('Commit Settings')
-        self.commit_button.clicked.connect(self.commit_settings)
-        layout.addWidget(self.commit_button)
+        # self.commit_button = QtWidgets.QPushButton('Commit Settings')
+        # self.commit_button.clicked.connect(self.commit_settings)
+        # layout.addWidget(self.commit_button)
 
         self.setLayout(layout)
         self.setWindowTitle('Stage Controller SHRC')
         self.show()
+        
+    def load_settings(self): 
+    
+        self.shrc.params['unit']['value'] = self.unit_input.currentText()
+        self.shrc.params['loop']['value'] = int(self.loop_input.value())
+        self.shrc.params['speed_ini']['value'] = self.speed_ini_input.value()
+        self.shrc.params['accel_t']['value'] = self.accel_t_input.value()
+        self.shrc.params['speed_fin']['value'] = self.speed_fin_input.value()
+        self.shrc.params['axis']['value'] = self.axis_input.currentText()
+        self.shrc.commit_settings()
+        print('Settings loaded')
 
 
 if __name__ == '__main__':
