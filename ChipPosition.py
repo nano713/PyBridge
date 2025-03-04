7b2d6be05eeb4be17d1ab7c92d440b7e80a014aa
adeshazer123 <adeshazer@wisc.edu>
2025-03-03 21:09:29 -0600

    Create ChipPosition. Create GUI based on inputting coordinates.

---
+++ ChipPosition.py
@@ -0,0 +1,23 @@
+# Takes coordinates from a Si chip from an optical microscope with the purpose to find the center of the chip
+# and the distance between the holes in the chip 
+
+
+import numpy as np
+import os 
+import pandas as pd 
+from ophyd import Component as Cpt
+from ophyd import SoftPositioner, PVPositioner
+from ophyd.pseudopos import (PseudoPositioner, PseudoSingle)
+
+
+class SiChipPosition(PseudoPositioner):
+    pos_x = Cpt(PVPositioner, limits=(-10, 10))
+    pos_y = Cpt(PVPositioner, limits=(-10, 10))
+    pos_z = Cpt(PseudoSingle, limits=(-10, 10))
+
+    def compute_center(self, x, y, z): 
+        """ Computes the center of the chip"""
+        center_x = np.mean(x)
+        center_y = np.mean(y)
+        center_z = np.mean(z)
+        return center_x, center_y, center_z
