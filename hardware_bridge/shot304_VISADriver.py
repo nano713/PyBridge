05b8f598cee3e1e0ddd865928b8c4f72076d9fe3
nano713 <50872819+nano713@users.noreply.github.com>
2025-03-07 23:18:44 +0900

    Update open_connection attributes. with https://jp.optosigma.com/en_jp/software__sample
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- hardware_bridge/shot304_VISADriver.py
+++ hardware_bridge/shot304_VISADriver.py
@@ -1,5 +1,6 @@
 #microscope hardware
-import pyvisa 
+import pyvisa
+import pyvisa.constants 
 
 class SHOT304VISADriver:
     def __init__(self, com):
@@ -7,14 +8,15 @@ class SHOT304VISADriver:
         self.rsrc_name = com
     
     def open_connection(self): 
-        self.shot304 = pyvisa.ResourceManager().open_resource(self.rsrc_name)
-        self._instr.baud_rate = 38400
-        self._instr.data_bits = 8 
-        self._instr.parity = pyvisa.constants.Parity.none 
-        self._instr.write_termination = "\r\n" 
-        self._instr.read_termination = "\r\n"
-        self._instr.flow_control.rts_cts = True #check documentation if true or false
-        self._instr.timeout = 50000
+        self.shot304 = pyvisa.ResourceManager().open_resource(self.rsrc_name) # "ASRL4::INSTR"
+        self.shot304.baud_rate = 38400
+        self.shot304.data_bits = 8 
+        self.shot304.parity = pyvisa.constants.Parity.none 
+        self.shot304.stop_bits = pyvisa.constants.StopBits.one
+        # self.shot304.write_termination = "\r\n" 
+        self.shot304.read_termination = "\r\n"
+        self.shot304.flow_control.rts_cts = True #check documentation if true or false
+        self.shot304.timeout = 5000
     
     def move(self, position, axis): 
         if position > 0:
@@ -23,8 +25,8 @@ class SHOT304VISADriver:
             self.shot304.write(f"A:{axis}-{abs(position)}")
         self.shot304.write("G:")
     
-    def get_position(self, axis):
-        return self.shot304.query(f"A:{axis}")
+    def get_position(self):
+        return self.shot304.query(f"Q:")
     
     def set_speed(self, speed_ini, speed_fin, acc, axis): 
         if 0 < speed_ini < 500000 and 0 < speed_fin < 500000 and 0 < acc < 1000:
