4665b299e1f9f292ab9821324c3dd6e9c7b66f11
Daichi Kozawa <kozawa.daichi@nims.go.jp>
2025-03-10 12:38:37 +0900

    Add suggestions to shot304 hardware code.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- hardware_bridge/shot304_VISADriver.py
+++ hardware_bridge/shot304_VISADriver.py
@@ -1,6 +1,6 @@
 #microscope hardware
 import pyvisa
-import pyvisa.constants 
+import pyvisa.constants # DK - this is included in `import pyvisa`
 
 class SHOT304VISADriver:
     def __init__(self, com):
@@ -30,15 +30,19 @@ class SHOT304VISADriver:
     
     def set_speed(self, speed_ini, speed_fin, acc, axis): 
         if 0 < speed_ini < 500000 and 0 < speed_fin < 500000 and 0 < acc < 1000:
-            self.shot304.write(f"S:{axis}{speed_ini}F{speed_fin}R{acc}")
+            self.shot304.write(f"D:{axis}S{speed_ini}F{speed_fin}R{acc}")
         else: 
             raise ValueError("Speed and acceleration values must be between 0 and 500000 and 0 and 1000, respectively.")
 
     def home(self): 
+        # DK - you need to specify the axis
         self.shot304.write("H:")
     
     def stop(self):
-        self.shot304.write("L: E")
+        # DK - it seems that L:E is an emergency stop. If this is normal stop, we should use a command that decelerates and stops.
+        self.shot304.write("L:E")
     
     def close_connection(self):
         self.shot304.close()
+
+    # Can we add a method to change the mode into remote (: host) mode?
