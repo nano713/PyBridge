f7c997be7398e26e45e2d62eb87b5b0c3e31b807
adeshazer123 <adeshazer@wisc.edu>
2025-03-16 20:50:51 -0500

    Update to add result button and compute

--- GUI/GUI_ChipPosition.py
+++ GUI/GUI_ChipPosition.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
 from MoveBridge.hardware_interface import SHRCStage
+from ChipPosition import SiChipPosition
 import logging
 from ophyd import Signal
 import pyvisa
@@ -59,25 +60,61 @@ class MyWindow(QtWidgets.QWidget):
         self.z1_input.setRange(-100, 100)
         self.z1_input.setValue(0)
         h1_layout.addWidget(self.z1_pos)
-        h1_layout.addWidget(self.z1_input) 
+        h1_layout.addWidget(self.z1_input)
+
+        self.x2_pos = QtWidgets.QLabel('X2 Position:')
+        self.x2_input = QtWidgets.QDoubleSpinBox()
+        self.x2_input.setRange(-100, 100)
+        self.x2_input.setValue(0)
+        h1_layout.addWidget(self.x2_pos)
+        h1_layout.addWidget(self.x2_input)
+
+        self.y2_pos = QtWidgets.QLabel('Y2 Position:')
+        self.y2_input = QtWidgets.QDoubleSpinBox()
+        self.y2_input.setRange(-100, 100)
+        self.y2_input.setValue(0)
+        h1_layout.addWidget(self.y2_pos)
+        h1_layout.addWidget(self.y2_input)
+
+        self.z2_pos = QtWidgets.QLabel('Z2 Position:')
+        self.z2_input = QtWidgets.QDoubleSpinBox()
+        self.z2_input.setRange(-100, 100)
+        self.z2_input.setValue(0)
+        h1_layout.addWidget(self.z2_pos)
+        h1_layout.addWidget(self.z2_input)
 
         layout.addLayout(h1_layout)
 
         self.compute_button = QtWidgets.QPushButton('Compute')
-        # self.compute_button.clicked.connect(self.compute)
+        self.compute_button.clicked.connect(self.compute)
         layout.addWidget(self.compute_button)
+
+        self.result_box = QtWidgets.QTextEdit()
+        self.result_box.setReadOnly(True)
+        layout.addWidget(self.result_box)
 
         self.setLayout(layout)
         self.show()
 
         def load_settings(self): 
             """ Loads the settings"""
-            self.x0 = self.x0_input.currentText()
-            self.y0 = self.y0_input.currentText()
-            self.z0 = self.z0_input.currentText()
-            self.x1 = self.x1_input.currentText()
-            self.y1 = self.y1_input.currentText()
-            self.z1 = self.z1_input.currentText()
+            self.chip_position = SiChipPosition()
+            self.x0 = self.x0_input.value()
+            self.y0 = self.y0_input.value()
+            self.z0 = self.z0_input.value()
+            self.x1 = self.x1_input.value()
+            self.y1 = self.y1_input.value()
+            self.z1 = self.z1_input.value()
+            self.x2 = self.x2_input.value()
+            self.y2 = self.y2_input.value()
+            self.z2 = self.z2_input.value()
+        def compute(self): 
+            """Computes the transformation matrix"""
+            self.load_settings()
+            transformation_matrix = self.chip_position.calculate_transformation_matrix(self.x0, self.y0, self.z0, self.x1, self.y1, self.z1, self.x2, self.y2, self.z2)
+            self.result_box.setText(str(transformation_matrix))
+            
+
 
 if __name__ == '__main__':
     import sys
