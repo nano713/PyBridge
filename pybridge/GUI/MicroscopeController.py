0259ed3c11276deecd4dcabba0e6a87d3a17da03
nano713 <50872819+nano713@users.noreply.github.com>
2025-04-02 22:09:22 +0900

    Update move_relateive increment.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- pybridge/GUI/MicroscopeController.py
+++ pybridge/GUI/MicroscopeController.py
@@ -16,6 +16,7 @@ class MicroscopeControl(QtWidgets.QWidget):
 
         # Create a grid layout
         grid_layout = QtWidgets.QGridLayout()
+        grid_layout_step = QtWidgets.QGridLayout()
 
         # Create arrow buttons
         self.up_button = QtWidgets.QPushButton('↑')
@@ -52,8 +53,18 @@ class MicroscopeControl(QtWidgets.QWidget):
         grid_layout.addWidget(self.axis_rotation_up_button, 0, 4)
         grid_layout.addWidget(self.axis_rotation_down_button, 2, 4)
 
+        # #TODO: ADD A STEP SIZE INPUT BUTTON
+        self.step_size_name = QtWidgets.QLabel('Step Size')
+        self.step_size_input = QtWidgets.QDoubleSpinBox()
+        self.step_size_input.setRange(1, 100000)
+        self.step_size_input.setSingleStep(1) 
+        self.step_size_input.setValue(10)
+        grid_layout_step.addWidget(self.step_size_name, 3, 0)
+        grid_layout_step.addWidget(self.step_size_input, 3, 1)
+        grid_layout.addLayout(grid_layout_step, 3, 0, 1, 2)
+        self.step_size_input.valueChanged.connect(self.update_step_size)
 
-        self.setLayout(grid_layout)
+
 
         # Connect buttons to functions
         self.up_button.clicked.connect(self.move_up)
@@ -65,9 +76,13 @@ class MicroscopeControl(QtWidgets.QWidget):
         self.axis_rotation_up_button.clicked.connect(self.move_axis_rotation_up)
         self.axis_rotation_down_button.clicked.connect(self.move_axis_rotation_down)
 
+
+        self.setLayout(grid_layout)
+        self.setLayout(grid_layout_step)
+
         self.setStyleSheet("""
             QWidget {
-                background-color:rgb(4, 52, 99);
+                background-color:rgb(4, 63, 122);
                 color:rgb(148, 148, 152);
             }
             QLineEdit {
@@ -98,16 +113,19 @@ class MicroscopeControl(QtWidgets.QWidget):
     def initialize(self):
         self.shot304 = SHOT304("ASRL3::INSTR")
         self.shot304.open_connection()
+    def update_step_size(self, value):
+        self.current_step_size = value
+        print(f"Step size updated to {self.current_step_size}")
 
     def move_up(self):
-        step_size = 10
+        step_size = int(self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 2)
             print(f"Move up {self.shot304.get_position(2)}")
         except Exception as e:
             print(f"Error moving up: {e}")
     def move_down(self):
-        step_size = -10
+        step_size =int(-self.step_size_input.value())
         try:
             self.shot304.move_relative(step_size, 2)
             print(f"Move down {self.shot304.get_position(2)}")
@@ -115,7 +133,7 @@ class MicroscopeControl(QtWidgets.QWidget):
             print(f"Error moving down: {e}") 
 
     def move_left(self):
-        step_size = -10
+        step_size = int(-self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 1)
             print(f"Move left {self.shot304.get_position(1)}")
@@ -123,7 +141,7 @@ class MicroscopeControl(QtWidgets.QWidget):
             print(f"Error moving left: {e}")
 
     def move_right(self):
-        step_size = 10
+        step_size = int(self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 1)
             print(f"Move right {self.shot304.get_position(1)}")
@@ -131,7 +149,7 @@ class MicroscopeControl(QtWidgets.QWidget):
             print(f"Error moving right: {e}")
     
     def move_z_up(self):
-        step_size = 10
+        step_size = int(self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 3)
             print(f"Move Z axis up {self.shot304.get_position(3)}")
@@ -139,7 +157,7 @@ class MicroscopeControl(QtWidgets.QWidget):
             print(f"Error moving Z up: {e}")
 
     def move_z_down(self):
-        step_size = -10
+        step_size = int(-self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 3)
             print(f"Move z axis down {self.shot304.get_position(3)}")
@@ -147,7 +165,7 @@ class MicroscopeControl(QtWidgets.QWidget):
             print(f"Error moving Z down: {e}")
 
     def move_axis_rotation_up(self):
-        step_size = 10
+        step_size = int(self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 4)
             print("Rotation up")
@@ -155,7 +173,7 @@ class MicroscopeControl(QtWidgets.QWidget):
             print(f"Error moving axis rotation up: {e}")
 
     def move_axis_rotation_down(self):
-        step_size = -1.0
+        step_size =int(-self.current_step_size)
         try:
             self.shot304.move_relative(step_size, 4)
             print("Rotation down")
