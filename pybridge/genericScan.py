d83cd548206ac17203edd08d2a9fcfe92ac317e8
nano713 <50872819+nano713@users.noreply.github.com>
2025-04-08 22:36:27 +0900

    Add method to load components.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- pybridge/genericScan.py
+++ pybridge/genericScan.py
@@ -1,13 +1,22 @@
 from bluesky import RunEngine
-from ophyd import Signal
+from ophyd import Signal, SignalRO
 from bluesky.plans import scan
+from bluesky.plans import grid_scan
+from bluesky.plans import count
+from bluesky.sim import motor1, motor2, motor3, motor4, motor5, motor6
 
 
 class GenericScan:
     def __init__(self, name_scan):
         self.RE = RunEngine()
         self.has_attribites(name_scan)
+
     def has_attribites(self, name_scan):
+        """Check if the class is either MoveBridge or ViewerBridge.
+        If not, raise an error.
+        Args:
+            name_scan: The class to check.
+        """
         try:
             class_string = str(name_scan)
         except Exception as e:
@@ -23,14 +32,18 @@ class GenericScan:
             raise ValueError(f"Unknown scan type: {name_scan}")
     
     def run_move(self, class_name):
-        components = []
-        for attr in dir(class_name):
-            if hasattr(class_name, attr) and isinstance(getattr(class_name, attr), Signal):
-                components.append(attr)
-        return components
+        class_done = class_name(name = "daichi doesn't know what to do")
+        components = list(class_done.component_names)
+        self.RE(grid_scan(class_done,
+                        motor1, ))
+  
         
 
     def run_viewer(self, class_name):
-        pass
+        class_done = class_name(name = "daichi doesn't know when to stop and listen at times")
+        components = list(class_done.component_names)
+        
+          
+
  
 
