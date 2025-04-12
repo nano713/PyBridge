6814ec3be9fc60f95173c23be5f316455121584d
Daichi Kozawa <kozawa.daichi@nims.go.jp>
2025-04-12 12:14:17 +0900

    Refactor run_move. Make grid_scan work.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- pybridge/genericScan.py
+++ pybridge/genericScan.py
@@ -7,11 +7,14 @@ from ophyd.sim import motor1, motor2, motor3#, motor4, motor5, motor6
 
 
 class GenericScan:
-    def __init__(self, name_scan, array ):
+    def __init__(self, name_scan, array):
         self.RE = RunEngine()
+      
+        
         self.array_scan = []
         self.motor = []
         self.array_scan.append(array)
+        print(self.array_scan)
         self.has_attribites(name_scan)
 
     def has_attribites(self, name_scan):
@@ -35,28 +38,55 @@ class GenericScan:
             raise ValueError(f"Unknown scan type: {name_scan}")
     
     def run_move(self, class_name):
+        from bluesky.callbacks.best_effort import BestEffortCallback
+        bec = BestEffortCallback()
+
+        # Send all metadata/data captured to the BestEffortCallback.
+        self.RE.subscribe(bec)  
+                
         class_done = class_name(name = "test")
-        components = list(class_done.component_names)
-        grid_scan_args = [class_done]
+        # components = list(class_done.component_names)
+        grid_scan_args = [[class_done]]
+        print("Error passed")
         for scan in self.array_scan: 
-            if len(self.array_scan) > 1:
-                motor = scan[0]
-                start,stop,step = scan[1:]
-                grid_scan_args.extend([motor, start, stop, step])
+            motor = scan[0]
+            start,stop,step = scan[1:]
+            grid_scan_args.append(motor)
+            grid_scan_args.append(start)
+            grid_scan_args.append(stop)
+            grid_scan_args.append(step)
+            print(grid_scan_args)
                 
         self.RE(grid_scan(*grid_scan_args)) # Extends the grid scan with dynamic motor and scan values
+        # grid_scan([motor1], motor1, 0, 10, 1, motor2, 0, 5, 0.5, motor3, 0, 20, 2)
  
          
 
-    def run_viewer(self, class_name):
-        class_done = class_name(name = "daichi doesn't know when to stop and listen at times")
-        components = list(class_done.component_names)
-        grid_scan_args = [class_done]
-        for scan in self.array_scan:
-            if len(scan) > 1:
-                motor = scan[0]
-                start,stop,step = scan[1:]
-                grid_scan_args.extend([motor, start, stop, step])
-        self.RE(scan(*grid_scan_args))
+    # def run_viewer(self, class_name):
+    #     class_done = class_name(name = "daichi doesn't know when to stop and listen at times")
+    #     components = list(class_done.component_names)
+    #     grid_scan_args = [class_done]
+    #     for scan in self.array_scan:
+    #         if len(scan) > 1:
+    #             motor = scan[0]
+    #             start,stop,step = scan[1:]
+    #             grid_scan_args.extend([motor, start, stop, step])
+    #     self.RE(scan(*grid_scan_args))
  
 
+if __name__ == "__main__":
+    from ophyd.sim import motor1, motor2, motor3
+    from pybridge.MoveBridge.shrcBridge import SHRCMoveBridge
+    from pybridge.genericScan import GenericScan
+    # Example usage
+    
+    class_name = SHRCMoveBridge # or "ViewerBridge"
+    # array_scan = [
+    #     [motor1, 0, 10, 1],
+    #     # [motor2, 0, 5, 0.5],
+    #     # [motor3, 0, 20, 2]
+    # ]
+
+    # scan = GenericScan(class_name, array_scan)
+    gene = GenericScan(SHRCMoveBridge, [motor1, 1,10,10])
+    print(gene.array_scan)
