0cbc564d7e21a4526a82c685603a74b93a0a6914
Daichi Kozawa <kozawa.daichi@nims.go.jp>
2025-04-19 12:28:22 +0900

    Refactor SHRCAxis class, readback, and setpoint.
    TODO: update the value when x.setpoint.get()
    
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- pybridge/MoveBridge/shrcBridge.py
+++ pybridge/MoveBridge/shrcBridge.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 
     
 class SHRCMoveBridge(PVPositioner): # Device
+    
 
     setpoint = Cpt(Signal) #target position
     readback = Cpt(SignalRO) #Read position
@@ -235,61 +236,64 @@ class SHRCMoveBridge(PVPositioner): # Device
     # def close_connection(self):
     #     self.shrc.close() 
 
-# class SHRCAxis(PVPositioner):
-#     def __init__(
-#         self,
-#         prefix="",
-#         *,
-#         limits=None,
-#         name=None,
-#         read_attrs=None,
-#         configuration_attrs=None,
-#         parent=None,
-#         egu="",
-#         axis = 1,
-#         **kwargs,
-#     ):
+class SHRCAxis(SHRCMoveBridge):
+    def __init__(
+        self,
+        prefix="",
+        *,
+        limits=None,
+        name=None,
+        read_attrs=None,
+        configuration_attrs=None,
+        parent=None,
+        egu="",
+        axis = 1,
+        driver = None,
+        **kwargs,
+    ):
+        super().__init__(
+            prefix=prefix,
+            read_attrs=read_attrs,
+            configuration_attrs=configuration_attrs,
+            name=name,
+            parent=parent,
+            **kwargs,
+        )
 
-#         # self.shrc = SHRCMoveBridge(name = "Daichi is a meanie :)")
-#         self.axis = axis
-#         self.shrc = SHRCMoveBridge.shrc
+        # self.shrc = SHRCMoveBridge(name = "Daichi is a meanie :)")
+        self.axis = axis
+        self.shrc = driver
+        self.axis_component.put(self.axis)
+        self.setpoint.put = self.move
+        self.readback.get = self.get_position
+        
     
 
-#     def call_axis(self):
-#         """Call the axis to set the axis of the SHRC
-#         Returns
-#            setpoint_value: The setpoint value of the axis"""
-#         self.shrc.set_axis(self.axis)
-#         return self.shrc.setpoint_value(self.axis)
+    # def call_axis(self):
+    #     """Call the axis to set the axis of the SHRC
+    #     Returns
+    #        setpoint_value: The setpoint value of the axis"""
+    #     return self.shrc.query_position(self.axis)
     
-#     def get_position(self):
-#         """Get the position of the axis
-#         Returns
-#             position: The position of the axis"""
-#         self.shrc.get_position(self.axis)
-#         return self.shrc.readback.get()
+    def get_position(self):
+        """Get the position of the axis
+        Returns
+            position: The position of the axis"""
+        return self.shrc.query_position(self.axis)
+        
            
-#     def move(self, position: float, wait=True, timeout=None):
-#         """Move the axis to the position
-#         Args
-#             position: The position to move to
-#             wait: Whether to wait for the move to finish
-#             timeout: The timeout for the move
-#         Returns
-#             status: The status of the move
-#         """
-#         self.shrc.setpoint.put(position)
-#         value = self.shrc.move(self.shrc.setpoint.get(), self.shrc.axis_component.get())
-#         print(value)
-#         status = MoveStatus(self, target = position, timeout = timeout, settle_time = self._settle_time)
-#         if value == 1: 
-#             self.shrc.done.put(value = True) #shrc successfully moved
-#             print("done true")
-#         else: 
-#             self.shrc.done.put(value = False)
-#             print("done false")
-#         status.set_finished()
-#         return status
+    def move(self, position: float, wait=True, timeout=None):
+        """Move the axis to the position
+        Args
+            position: The position to move to
+            wait: Whether to wait for the move to finish
+            timeout: The timeout for the move
+        Returns
+            status: The status of the move
+        """
+        # self.setpoint.put(position)
+        value = self.shrc.move(position, self.axis)
+        
     
 
 # For PYQT5, we need to load widgets and text to have the commit_settings to load in the GUI
@@ -368,3 +372,12 @@ if __name__ == "__main__":
     # print(f"keithley.voltage: {keithley.voltage.get()}")
     # plt.show()
     
+    if __name__ == "__main__":
+        from pybridge.hardware_bridge.shrc203_VISADriver import SHRC203VISADriver
+        from pybridge.MoveBridge.shrcBridge import SHRCMoveBridge, SHRCAxis
+        # shrc = SHRCMoveBridge(name="shrc203")
+
+        shrc = SHRC203VISADriver(rsrc_name="ASRL3::INSTR")
+        x = SHRCAxis(axis = 1, driver = shrc, name = "x")
+        y = SHRCAxis(axis = 2, driver = shrc, name = "y")
+        z = SHRCAxis(axis = 3, driver = shrc, name = "z")
