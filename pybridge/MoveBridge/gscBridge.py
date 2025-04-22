3f5dfadf894a56888acd65ed028dc107dc6e886c
nano713 <50872819+nano713@users.noreply.github.com>
2025-04-22 11:48:47 +0900

    Add parameters.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- pybridge/MoveBridge/gscBridge.py
+++ pybridge/MoveBridge/gscBridge.py
@@ -1,5 +1,42 @@
 from ophyd.signal import Signal, SignalRO
+from ophyd.device import Device, Component as Cpt
+from pybridge.hardware_bridge.gsc_VISADriver import GSC
+from ophyd.pv_positioner import PVPositioner
 
-class GSCMoveBridge:
-    def __init__(self, com):
-        self.gsc = com
+class GSCMoveBridge(PVPositioner):
+    setpoint = Cpt(Signal) #target position
+    readback = Cpt(SignalRO) #Read position
+    done = Cpt(Signal, value = False) #Instrument is done moving
+    actuate = Cpt(Signal) #Request to move
+    stop_signal =  Cpt(Signal) #Request to stop
+
+    axis_component = Cpt(Signal, value=1, kind="config")
+    speed_ini = Cpt(Signal, value=2000, kind="config")
+    speed_fin = Cpt(Signal, value=20000, kind="config")
+    accel_t = Cpt(Signal, value=100, kind="config")
+    # store_position = Cpt(Signal, value=0.0, kind="hinted")
+    loop = Cpt(Signal, value=0, kind="config")
+    unit = Cpt(Signal, value="um", kind="config")
+
+    def __init__(
+        self,
+        prefix="",
+        *,
+        limits=None,
+        name=None,
+        read_attrs=None,
+        configuration_attrs=None,
+        parent=None,
+        egu="",
+        driver = None,
+        **kwargs,
+    ):
+        super().__init__(
+            prefix=prefix,
+            read_attrs=read_attrs,
+            configuration_attrs=configuration_attrs,
+            name=name,
+            parent=parent,
+            **kwargs,
+        )
+        self.gsc = GSC(driver)
