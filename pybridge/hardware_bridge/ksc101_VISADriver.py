28470d5fdbcda8475e258b4f63a51a6860dbe14d
Daichi Kozawa <kozawa.daichi@nims.go.jp>
2025-05-09 22:35:04 +0900

    Test and debug.  Refactor get_shutter_state. Add SetOperatingMode to connect method.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- pybridge/hardware_bridge/ksc101_VISADriver.py
+++ pybridge/hardware_bridge/ksc101_VISADriver.py
@@ -23,38 +23,45 @@ Device.DeviceManagerCLI.BuildDeviceList()
 serial_numbers = [str(ser) for ser in Device.DeviceManagerCLI.GetDeviceList(Solenoid.KCubeSolenoid.DevicePrefix)]
 
 class KSC101:
-    def __init__(self):
+    def __init__(self, serial='68800739'):
         self._solenoid = None
+        self.serial = serial
     
     def connect(self):
-        self._solenoid = (Solenoid.KCubeSolenoid.CreateKCubeSolenoid(serial_numbers[0]))
-        self._solenoid.Connect(serial_numbers[0]) 
-        self._solenoid.WaitForSettingsInitialized(5000)
-        self._solenoid.StartPolling(100) 
-        self._solenoid.EnableDevice()
-        self._solenoid.GetSolenoidConfiguration(serial_numbers[0])
-    
-    def set_state(self, state):
-        pass 
-
-    # def get_cycle(self):
-    #     return self._solenoid.Cycle()
-    
-    def set_operating_mode(self, mode):
-        if mode == "Manual":
+        if self.serial in serial_numbers:
+            self._solenoid = (Solenoid.KCubeSolenoid.CreateKCubeSolenoid(self.serial))
+            self._solenoid.Connect(serial_numbers[0]) 
+            self._solenoid.WaitForSettingsInitialized(5000)
+            self._solenoid.StartPolling(100) 
+            self._solenoid.EnableDevice()
+            self._solenoid.GetSolenoidConfiguration(self.serial)
             self._solenoid.SetOperatingMode(Solenoid.SolenoidStatus.OperatingModes.Manual)
         
+        else:
+            raise ConnectionError("KSC101 not connected. Serial Number input is not in list")
+     
     def set_rotation(self):
         self._solenoid.TriggerRotation()
 
-    def disconnect(self):
-        self._solenoid.Disconnect()
+    def get_shutter_state(self):
+        value = self._solenoid.GetOperatingState()
+        value = value.value__
+        if value == 1:
+            return "Active"
+        elif value == 2:
+            return "Inactive"
+        else:
+            raise ValueError("Value is unknown. Check again if Device is connected")
     
     def open_shutter(self):
         self._solenoid.SetOperatingState(Solenoid.SolenoidStatus.OperatingStates.Active)
-
     
     def close_shutter(self):
         self._solenoid.SetOperatingState(Solenoid.SolenoidStatus.OperatingStates.Inactive)
+
+    def disconnect(self):
+        self._solenoid.StopPolling()
+        self._solenoid.Disconnect()
+    
 
 
